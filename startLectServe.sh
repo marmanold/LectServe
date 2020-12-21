1ecfda203833b0ecbfcfaf0caf2cd7160b2bc7ff
Michael W. Arnold <michael@rnold.info>
2020-12-21 13:53:03 -0600

    Further Improvements

--- startLectServe.sh
+++ startLectServe.sh
@@ -3,10 +3,10 @@
 ## VERSION 1.20201221 ##
 
 export PERL5OPT=-MTry::Tiny::Tiny
-git pull --rebase --depth 1 --prune
+git pull --rebase --prune
 carton install
 echo "Stopping old version..."
-kill -1 $HOME/lectserve.pid
+kill -1 `cat $HOME/lectserve.pid`
 echo "Starting new LectServe version..."
 cd $HOME/LectServe
 carton exec starman --port 5000 $HOME/LectServe/bin/app.psgi --daemonize --pid $HOME/lectserve.pid
